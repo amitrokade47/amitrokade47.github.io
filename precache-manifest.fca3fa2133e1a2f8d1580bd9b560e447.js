1a0e6f7f0f28e2c4c579496c470f24e04188164e
Amit Rokade <amitrokade@Amits-MacBook-Pro.local>
2020-11-11 19:56:49 -0500

    Updates

---
+++ precache-manifest.fca3fa2133e1a2f8d1580bd9b560e447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba9b6af7eabe9bea44fe",
+    "url": "/me/static/css/main.5d7287c5.chunk.css"
+  },
+  {
+    "revision": "ba9b6af7eabe9bea44fe",
+    "url": "/me/static/js/main.ba9b6af7.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "356b49ec4d4864dd6084",
+    "url": "/me/static/js/2.356b49ec.chunk.js"
+  },
+  {
+    "revision": "8978d6d5edd7ab5973435bb9c7cd0308",
+    "url": "/me/index.html"
+  }
+];
